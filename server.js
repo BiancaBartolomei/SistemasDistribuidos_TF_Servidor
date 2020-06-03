bbdbe1a8f308a9afff21c00a3a7ef6bc4ae6c950
Bianca <biancarbartolomei@gmail.com>
2020-06-03 16:01:25 -0300

    [ADD] Changes in pool request

--- server.js
+++ server.js
@@ -1,32 +1,46 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000; //porta padrão
+const port = 3300; //porta padrão
 const { Pool, Client } = require('pg')
-
-
+const connectionString = 'postgresql://postgres:19972015@localhost:5432/trabalho_sd'
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'trabalho_sd',
-    password: '',
-    port: 5432,
-  })
+  connectionString: connectionString,
+})
+
+
+
 //configurando o body parser para pegar POSTS mais tarde
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+
+
 
 //definindo as rotas
 const router = express.Router();
 router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
 app.use('/', router); // requisições que chegam na raiz devem ser enviadas para o router
 
-router.get('/users', (req, res) =>{
-    pool.query('SELECT * FROM users', (err, res) => {
-        console.log(err, res)
-        pool.end()
-      })
-   
+router.get('/users', (req, response) =>{
+  pool.query('SELECT * FROM users', (err, res) => {
+    pool.end()
+    response.json(res.rows)
+
+  })
+  
+})
+
+router.post('/createUser', (req, response) => {
+  const text = 'INSERT INTO public.users(user_id, name, cpf, email, password) VALUES ($1, $2, $3, $4, $5)'
+  const values = [1, 'aaaaa', '122222', 'brian.m.carlson@gmail.com','15454']
+
+  pool.query(text, values, (err, res) => {
+    pool.end()
+    response.json(res.rows)
+
+  })
 })
 
 //inicia o servidor
