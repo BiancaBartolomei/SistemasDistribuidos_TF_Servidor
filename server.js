56876a64c0be0b404b8026ec78d8db033d6c7c9f
Luis Malta <luiso.malta@gmail.com>
2020-06-09 18:12:07 -0300

    [ADD] Client request

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = 3300; //porta padrão
 const { Pool, Client } = require('pg')
-const connectionString = 'postgresql://postgres:19972015@localhost:5432/trabalho_sd'
-const pool = new Pool({
+const connectionString = 'postgresql://postgres:suasenha@localhost:5432/trabalho_sd'
+const pool = new Client({
   connectionString: connectionString,
 })
+
+pool.connect()
 
 
 
@@ -37,10 +39,62 @@ router.post('/createUser', (req, response) => {
   const values = [1, 'aaaaa', '122222', 'brian.m.carlson@gmail.com','15454']
 
   pool.query(text, values, (err, res) => {
+    // pool.end()
+    response.json(res)
+
+  })
+})
+
+router.delete('/deleteUsers/:id', (req, response) =>{
+  const id = parseInt(req.params.id);
+  pool.query(`DELETE FROM users where id=${id}`, (err, res) => {
     pool.end()
     response.json(res.rows)
 
   })
+  
+})
+
+router.patch('/place/:id', (req, response) =>{
+  const id = parseInt(req.params.id);
+  const name = req.body.name.substring(0,150);
+  const cnpj = req.body.cnpj.substring(0,11);
+  const area = parseInt(req.body.area.substring(0,50));
+  const max_qnt = req.body.max_qnt.substring(0,50);
+  
+  pool.query(`UPDATE places SET name='${name}',cnpj='${cnpj}',area=${area},max_qnt='${max_qnt}' WHERE id=${id}`, (err, res) => {
+      // pool.end()
+      response.json(res)
+      console.log(res)
+    })
+ 
+})
+
+
+router.get('/place/:name', (req, response) =>{
+  pool.query(`SELECT * FROM places WHERE similarity(name, '${req.params.name}') > 0.5`, (err, res) => {
+      // pool.end()
+      response.json(res.rows)
+    })
+    console.log("Get places")
+ 
+})
+
+router.post('/request', (req, response) =>{
+  const id = parseInt(req.params.id);
+  console.log(req.body.name)
+  const user_id = req.body.user;
+  const name = req.body.name;
+  const cnpj = req.body.cnpj;
+  const area = parseInt(req.body.area);
+  const max_qnt = req.body.max_qnt;
+  console.log(name)
+  
+  pool.query(`INSERT INTO requests VALUES (id=${id}, user_id='${user_id}', name='${name}',cnpj='${cnpj}',area=${area},max_qnt='${max_qnt}')`, (err, res) => {
+      // pool.end()
+      response.json(res)
+    })
+ 
 })
 
 //inicia o servidor
